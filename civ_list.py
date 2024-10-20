e109b431bbd11d14310a4a3d6addfc96785f2eaf
jonrmuk <99203971+jonrmuk@users.noreply.github.com>
2024-10-20 14:57:16 +0100

    civ list updated

--- civ_list.py
+++ civ_list.py
@@ -13,9 +13,9 @@ civ_list = [
     discord.SelectOption(label="Arabia_Sultan",
                          emoji="<:ArabiaSultan:1065754545470324897>"),
 
-    discord.SelectOption(label="Aztec_Montezuma",
-                         emoji="<:AztecMontezuma:1065754555024937090>"),
-
+    discord.SelectOption(label="Egypt_Ramses",
+                         emoji="<:EgyptRamses:1076186907668648010>"),
+    
     discord.SelectOption(label="China_Qin-Unifier",
                          emoji="<:ChinaQinUnifier:1065754582816395436>"),
 
@@ -34,6 +34,55 @@ civ_list = [
     discord.SelectOption(label="France_Eleanor",
                          emoji="<:FranceEleanor:1065756606102503434>"),
 
+    discord.SelectOption(label="Ottoman_Muhtesem",
+                         emoji="<:OttomanMuhtesem:1065811503514648576>"),
+
+    discord.SelectOption(label="Scotland_Robert-The-Bruce",
+                        emoji="<:ScotlandRobertTheBruce:1065811537303969913>"),
+
+    discord.SelectOption(label="Abraham-Licoln",
+                        emoji="<:AmericaAbrahamLincoln:1065754537178181661>"),
+
+    discord.SelectOption(label="Sweden",
+                        emoji="<:SwedenKristina:1065811555435945994>"),
+
+    discord.SelectOption(label="Kongo_Mbande",
+                        emoji="<:KongoNzingaMbande:1065756652797689898>"),
+
+    discord.SelectOption(label="Inca-Pachacuti",
+                        emoji="<:IncaPachacuti:1065756628630118400>"),
+
+    discord.SelectOption(label="Greece_Pericles",
+                        emoji="<:GreecePericles:1065756622586138705>"),
+
+    discord.SelectOption(label="Gran-Colombia_Simon-Bolivar",
+                         emoji="<:GranColombiaSimonBolivar:1065754592182272030>"),
+
+    discord.SelectOption(label="Nubia",
+                        emoji="<:NubiaAmanitore:1065811497181261894>"),
+
+    discord.SelectOption(label="Khmer_Jayavarman",
+                        emoji="<:KhmerJayavarmanVII:1065756647630311444>"),
+
+    discord.SelectOption(label="Tamar_Georgia",
+                        emoji="<:GeorgiaTamar:1065756611865493605>"),
+
+    discord.SelectOption(label="Kublai_China",
+                        emoji="<:ChinaKublaiKhan:1065754575824494745>"),
+
+    discord.SelectOption(label="Australia",
+                        emoji="<:AustraliaJohnCurtin:1065754551900188703>"),
+
+    discord.SelectOption(label="Wilhelmina_Netherlands",
+                        emoji="<:DutchWilhelmina:1065754599077707807>"),
+
+    discord.SelectOption(label="Harald_Varangian",
+                        emoji="<:HaraldHardradaVarangian:1090727009396731914>"),
+
+]
+
+civ_list_unused = [
+    
     discord.SelectOption(label="Greece_Gorgo",
                          emoji="<:GreeceGorgo:1065756618928701440>"),
 
@@ -45,6 +94,15 @@ civ_list = [
 
     discord.SelectOption(label="Korea_Sejong",
                          emoji="<:Sejong:1085884319907713087>"),
+
+    discord.SelectOption(label="Mapuche_Lautaro",
+                         emoji="<:MapucheLautaro:1065756670652854302>"),
+
+    discord.SelectOption(label="Zulu_Shaka",
+                         emoji="<:ZuluShaka:1065811565904936991>")
+
+    discord.SelectOption(label="Aztec_Montezuma",
+                         emoji="<:AztecMontezuma:1065754555024937090>"),
 
     discord.SelectOption(label="Korea_Seondeok",
                          emoji="<:KoreaSeondeok:1065756656476098672>"),
@@ -58,9 +116,6 @@ civ_list = [
     discord.SelectOption(label="Ottoman_Suleiman",
                          emoji="<:OttomanSuleiman:1065811501052596378>"),
 
-    discord.SelectOption(label="Ottoman_Muhtesem",
-                         emoji="<:OttomanMuhtesem:1065811503514648576>"),
-
     discord.SelectOption(label="Persia_Cyrus",
                          emoji="<:PersiaCyrus:1065811506702327919>"),
 
@@ -70,28 +125,7 @@ civ_list = [
     discord.SelectOption(label="Rome_Caesar",
                          emoji="<:RomeCaesar:1065811525119528981>"),
 
-    discord.SelectOption(label="Scotland_Robert-The-Bruce",
-
-                         emoji="<:ScotlandRobertTheBruce:1065811537303969913>"),
-
     discord.SelectOption(label="Sumeria_Gilgamesh",
                          emoji="<:SumeriaGilgamesh:1065811548444049518>")
-
-
-]
-
-civ_list_unused = [
-
-    discord.SelectOption(label="Egypt_Ramses",
-                         emoji="<:EgyptRamses:1076186907668648010>"),
-
-    discord.SelectOption(label="Gran-Colombia_Simon-Bolivar",
-                         emoji="<:GranColombiaSimonBolivar:1065754592182272030>"),
-
-    discord.SelectOption(label="Mapuche_Lautaro",
-                         emoji="<:MapucheLautaro:1065756670652854302>"),
-
-    discord.SelectOption(label="Zulu_Shaka",
-                         emoji="<:ZuluShaka:1065811565904936991>")
 
 ]
